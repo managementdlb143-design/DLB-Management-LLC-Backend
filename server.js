f652dee3e6a9165e4daacf857dc1f2a6517ab90e
MrBeast273 <mrbeast9032@gmail.com>
2026-07-30 23:48:55 +0500

    Fix carrierRoutes module export format

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Serve uploaded files statically
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
